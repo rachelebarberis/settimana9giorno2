debf2e0283db9da3335f40c74c6a5c1925b16724
Rachele Barberis <rachele.barberis.12@gmail.com>
2025-01-08 16:02:11 +0100

    commit

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -5,12 +5,12 @@ const AllTheBooks = function () {
         <Container className="mt-4">
             <Row>
                 {books.map((book, i) => (
-                    <Col xs={12} lg={3} key={i} className="mb-4">
-                        <Card>
-                            <Card.Img variant="top" src={book.img} alt={book.title} />
+                    <Col xs={6} lg={3} key={i} className="mb-4">
+                        <Card className='p-1 border-2 border-muted rounded-2 h-100'>
+                            <Card.Img src={book.img} alt={book.title} className='h-100' />
                             <Card.Body>
-                                <Card.Title>{book.title}</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='text-truncate text-primary'>{book.title}</Card.Title>
+                                <Card.Text className='text-primary'>
                                     Prezzo: €{book.price}
                                 </Card.Text>
                             </Card.Body>

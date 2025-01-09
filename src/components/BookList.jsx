90e9e6d4829bcc1fc4a4f258c3c9a1cf0f7f02a3
Rachele Barberis <rachele.barberis.12@gmail.com>
2025-01-09 15:51:49 +0100

    commit

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,13 +1,34 @@
 /* eslint-disable react/prop-types */
-
+import { useState } from "react";
 import SingleBook from "./SingleBook";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Form } from "react-bootstrap";
 
 const BookList = ({ books }) => {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+
+  const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <Container >
+    <Container>
+      <Form.Control
+        type="text"
+        placeholder="Cerca nella libreria"
+        value={searchQuery}
+        onChange={handleSearchChange}
+        className="mb-3"
+      />
+      
       <Row>
-        {books.map((book) => (
+        {filteredBooks.map((book) => (
           <Col key={book} sm={6} md={2} lg={2} className="mb-2">
             <SingleBook book={book} />
           </Col>
@@ -18,3 +39,4 @@ const BookList = ({ books }) => {
 };
 
 export default BookList;
+
